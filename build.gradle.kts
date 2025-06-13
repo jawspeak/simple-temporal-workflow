d665f0d142c6dfd2d1f977bde147d32a3eaf5cbc
Jonathan Andrew Wolter <44742+jawspeak@users.noreply.github.com>
2025-06-13 10:59:37 -0400

    refactor: gradle version of temporal

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,11 @@ repositories {
     mavenCentral()
 }
 
+val temporalVersion = "1.20.0"
+
 dependencies {
     // https://github.com/temporalio/sdk-java/releases, https://central.sonatype.com/artifact/io.temporal/temporal-sdk?smo=true
-    implementation("io.temporal:temporal-sdk:1.29.0")
+    implementation("io.temporal:temporal-sdk:${temporalVersion}")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
@@ -40,7 +42,7 @@ testing {
                 implementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.22")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
                 implementation("io.mockk:mockk:1.13.9")
-                implementation("io.temporal:temporal-testing:1.29.0")
+                implementation("io.temporal:temporal-testing:${temporalVersion}")
             }
         }
     }
